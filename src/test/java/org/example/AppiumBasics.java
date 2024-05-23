005f6d6511d76f0474c9a13b9c883e6ba690f14d
J-Potes <jpotes.multimedia@gmail.com>
2024-05-23 08:54:48 -0500

    APK file added

--- src/test/java/org/example/AppiumBasics.java
+++ src/test/java/org/example/AppiumBasics.java
@@ -13,10 +13,15 @@ import java.net.URL;
 public class AppiumBasics {
     @Test
     public void appiumTest() throws MalformedURLException, URISyntaxException {
-        //Android Driver or iOS Driver
+
+        // Specify capabilities
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Pixel 4a API 33"); //Emulator
 
+        // First create the object for Android Driver or iOS Driver
+        // In this case Android Driver UiAutomator2
+        // Appium code -> Appium Server -> Mobile
+        // Parameters: ...new AndroidDriver( Appium server URL, Capabilities )
         AndroidDriver driver = new AndroidDriver(new URI("https://127.0.0.1:4723").toURL(), options);
     }
 }
