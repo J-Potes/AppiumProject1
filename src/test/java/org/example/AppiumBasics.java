c35dde818684f2319ef1b86a3e4129cfc313fd1c
J-Potes-GLB <juan.potes@globant.com>
2024-06-04 00:06:02 -0500

    Changes to work in another device

--- src/test/java/org/example/AppiumBasics.java
+++ src/test/java/org/example/AppiumBasics.java
@@ -16,13 +16,24 @@ public class AppiumBasics {
 
         // Specify capabilities
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("emulator-5554"); //Emulator
-        options.setApp("D:\\Users\\ASUS\\Desktop\\Cursos\\3_Appium Mobile Automation\\AppiumProject1\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+
+        // PERSONAL C
+        //options.setDeviceName("emulator-5554"); //Emulator
+        //options.setApp("D:\\Users\\ASUS\\Desktop\\Cursos\\3_Appium Mobile Automation\\AppiumProject1\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+
+        // WORK C
+        options.setDeviceName("adb-ccb76178-Fhy4eo._adb-tls-connect._tcp"); //Device physical
+        options.setApp("C:\\Users\\juan.potes\\Documents\\Cursos\\Appium\\AppiumProject1\\src\\test\\java\\resources\\ApiDemos-debug.apk");
 
         // First create the object for Android Driver or iOS Driver
         // In this case Android Driver UiAutomator2
         // Appium code -> Appium Server -> Mobile
         // Parameters: ...new AndroidDriver( Appium server URL, Capabilities )
-        AndroidDriver driver = new AndroidDriver(new URI("http://192.168.0.11:4723/").toURL(), options);
+
+        // PERSONAL C
+        // AndroidDriver driver = new AndroidDriver(new URI("http://192.168.0.11:4723/").toURL(), options);
+
+        // WORK C
+        AndroidDriver driver = new AndroidDriver(new URI("http://192.168.0.14:4723/").toURL(), options);
     }
 }
